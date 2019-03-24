fe6521aaab9ad335aa99536718b5a02ad7223842
David Baumgold <david@davidbaumgold.com>
2019-03-24 10:05:46 +0100

    maybe this?

--- app/views/auth.py
+++ app/views/auth.py
@@ -74,7 +74,7 @@ def merge():
 
 def merge_users(merge_into, merge_from):
     assert merge_into != merge_from
-    OAuth.query.filter_by(user=merge_from).update({OAuth.user: merge_into})
+    OAuth.query.filter_by(user=merge_from).update({"user_id": merge_into.id})
     db.session.delete(merge_from)
     db.session.commit()
     return merge_into
